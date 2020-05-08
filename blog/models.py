4db907758f6e6ba19b2107ce6573cb32a4ff1690
Ashish Kumar Mandal <666kmandal@gmail.com>
2020-05-08 10:32:18 +0530

    add no of views

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,7 @@ class Post(models.Model):
     content = models.TextField()
     author = models.CharField(max_length=50)
     slug = models.CharField(max_length=150)
+    views = models.IntegerField(default=0) 
     timeStamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -19,9 +20,9 @@ class BlogComment(models.Model):
     sno = models.AutoField(primary_key=True)
     comment = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)    
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)  
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
     timeStamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.comment[0:15] + "... by- " + self.user.username
+        return self.comment[0:15] + "... by- " + self.user.usernamepython 
